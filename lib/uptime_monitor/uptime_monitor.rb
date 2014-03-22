17844950d26feb33b71f6343451a5afa26da8d6a
The Rainmaker <obioraakubue@yahoo.com>
2014-03-22 17:27:35 -0400

    added complete specs for the uptime_monitor

--- lib/uptime_monitor/uptime_monitor.rb
+++ lib/uptime_monitor/uptime_monitor.rb
@@ -5,17 +5,24 @@ module Ragios
       attr_reader :test_result
       attr_reader :success
 
+      def initialize
+        @test_result = ActiveSupport::OrderedHash.new
+      end
+
       def init(monitor)
         @monitor = OpenStruct.new(monitor)
-        raise "A url must be provided for uptime_monitor in #{@monitor.monitor} monitor" if @monitor.url.nil?
+        message = "A url must be provided for uptime_monitor: #{@monitor.monitor}"
+        raise(Hercules::UptimeMonitor::NoUrlProvided.new(error: message), message) if @monitor.url.nil?
+        message = "A browser must be provided for uptime_monitor: #{@monitor.monitor}"
+        raise(Hercules::UptimeMonitor::NoBrowserProvided.new(error: message), message) if @monitor.browser.nil?
+        message = "A validation (exists?) must be provided for uptime_monitor: #{@monitor.monitor}"
+        raise(Hercules::UptimeMonitor::NoValidationProvided.new(error: message), message) if @monitor.exists?.nil?
       end
 
       def test_command?
-        @test_result = ActiveSupport::OrderedHash.new
-        return true unless @monitor.exists?
         @success = true
         browser_reader = Hercules::UptimeMonitor::BrowserReader.new(@monitor.browser)
-        @browser = start_browser(@monitor.url, browser_reader.browser_name, browser_reader.headless)
+        start_browser(@monitor.url, browser_reader.browser_name, browser_reader.headless)
         exists(@monitor.exists?)
         close_browser
         @success
@@ -29,7 +36,7 @@ module Ragios
       end
 
       def start_browser(url, browser_name, headless)
-        Hercules::UptimeMonitor::Browser.new(url, browser_name, headless)
+        @browser = Hercules::UptimeMonitor::Browser.new(url, browser_name, headless)
       end
 
       def exists(page_elements)
@@ -44,5 +51,12 @@ module Ragios
         @test_result.merge!(result)
       end
     end
+  end
+end
+module Hercules
+  module UptimeMonitor
+    class NoUrlProvided < StandardError; end
+    class NoValidationProvided < StandardError; end
+    class NoBrowserProvided < StandardError; end
   end
 end
--- spec/uptime_monitor_spec.rb
+++ spec/uptime_monitor_spec.rb
@@ -1,1 +1,69 @@
 require 'spec_helper'
+
+describe Ragios::Plugin::UptimeMonitor do
+  before(:each) do
+    @uptime_monitor = Ragios::Plugin::UptimeMonitor.new
+  end
+  it "sets the correct test result for success" do
+    @uptime_monitor.result!("page_element", state = true)
+    @uptime_monitor.test_result.should == {"page_element" => :exists_as_expected}
+    @uptime_monitor.success.should == true
+  end
+  it "sets the correct test result for failure" do
+    @uptime_monitor.result!(:page_element, state = false)
+    @uptime_monitor.test_result.should == {:page_element => :does_not_exist_as_expected}
+    @uptime_monitor.success.should == false
+  end
+  it "must include a url" do
+    monitor = {exists?: :exists, browser: :browser}
+    expect{@uptime_monitor.init(monitor)}.to raise_error(Hercules::UptimeMonitor::NoUrlProvided)
+  end
+  it "must include a browser" do
+    monitor = {exists?: :exists, url: :url}
+    expect{@uptime_monitor.init(monitor)}.to raise_error(Hercules::UptimeMonitor::NoBrowserProvided)
+  end
+  it "must include a validation" do
+    monitor = {browser: :browser, url: :url}
+    expect{@uptime_monitor.init(monitor)}.to raise_error(Hercules::UptimeMonitor::NoValidationProvided)
+  end
+  it "returns true if page element exists" do
+    monitor = {browser: :browser, exists?: :exists, url: :url}
+    @uptime_monitor.init(monitor)
+    @uptime_monitor.start_browser("http://obi-akubue.org","firefox", headless = true)
+    page_element = [:title]
+    @uptime_monitor.exists([page_element])
+    @uptime_monitor.test_result.should == {page_element => :exists_as_expected}
+    @uptime_monitor.success.should == true
+    @uptime_monitor.close_browser
+  end
+  it "returns false if page element don't exists" do
+    monitor = {browser: :browser, exists?: :exists, url: :url}
+    @uptime_monitor.init(monitor)
+    @uptime_monitor.start_browser("http://obi-akubue.org","firefox", headless = true)
+    page_element = [:title, [text: "dont_exist"]]
+    @uptime_monitor.exists([page_element])
+    @uptime_monitor.test_result.should == {page_element => :does_not_exist_as_expected}
+    @uptime_monitor.success.should == false
+    @uptime_monitor.close_browser
+  end
+  it "runs a test that passes" do
+    page_element = [:title]
+    monitor = {url: "http://obi-akubue.org",
+                browser: ["firefox", headless: true],
+                exists?: [page_element]
+              }
+    @uptime_monitor.init(monitor)
+    @uptime_monitor.test_command?.should == true
+    @uptime_monitor.test_result.should == {page_element => :exists_as_expected}
+  end
+  it "runs a test that fails" do
+    page_element = [:title, [text: "dont_exist"]]
+    monitor = {url: "http://obi-akubue.org",
+                browser: ["firefox", headless: true],
+                exists?: [page_element]
+              }
+    @uptime_monitor.init(monitor)
+    @uptime_monitor.test_command?.should == false
+    @uptime_monitor.test_result.should == {page_element => :does_not_exist_as_expected}
+  end
+end
