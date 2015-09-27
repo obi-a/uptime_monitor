4278545f6d48f64ea2a4d94fc8db63e3d0a5e4c5
Obi Akubue <obioraakubue@yahoo.com>
2015-09-27 16:55:05 -0400

    added parsers & grammars for transactions and browsers

---
+++ lib/uptime_monitor/parser.rb
@@ -0,0 +1,45 @@
+require 'treetop'
+
+module Hercules
+  module UptimeMonitor
+    class Parser
+      def self.parse(data, parser)
+        if data.respond_to? :read
+          data = data.read
+        end
+
+        ast = parser.parse data
+
+        if ast
+          return ast.content
+        else
+          parser.failure_reason =~ /^(Expected .+) after/m
+          message =
+          "#{$1.gsub("\n", '$NEWLINE')}:" << "\n" <<
+          data.lines.to_a[parser.failure_line - 1] << "\n" <<
+          "#{'~' * (parser.failure_column - 1)}^"
+          raise(Hercules::UptimeMonitor::SyntaxError.new(error: message), message)
+        end
+      end
+
+    end
+
+    class MaestroLangParser
+      Treetop.load(File.expand_path(File.join(File.dirname(__FILE__), 'maestro')))
+      @@maestro_parser = MaestroParser.new
+      def parse(data)
+        Hercules::UptimeMonitor::Parser.parse(data, @@maestro_parser)
+      end
+    end
+
+    class BrowsersLangParser
+      Treetop.load(File.expand_path(File.join(File.dirname(__FILE__), 'browsers')))
+      @@browsers_parser = BrowsersParser.new
+      def parse(data)
+        Hercules::UptimeMonitor::Parser.parse(data, @@browsers_parser)
+      end
+    end
+
+    class SyntaxError < StandardError; end
+  end
+end
